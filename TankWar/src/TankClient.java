13d6df172f4c0e05108d67b8e57c13c86e3bf544
George <zhouzhihui1988@gmail.com>
2016-10-07 23:41:44 +0800

    TankWar v0.4

--- TankWar/src/TankClient.java
+++ TankWar/src/TankClient.java
@@ -2,14 +2,13 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class TankClient extends Frame{
-	
+	int x = 50,y = 50;
 	public void launchFrame(){	
 		this.setLocation(400, 300);
 		this.setSize(800, 600);
+		this.setBackground(Color.GREEN);
 		this.setTitle("TankWar");
 		this.addWindowListener(new WindowAdapter(){
-
-			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
@@ -17,11 +16,41 @@ public class TankClient extends Frame{
 		});
 		this.setResizable(false);
 		this.setVisible(true);
+		
+		new Thread(new PaintThread()).start();
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.RED);
+		g.fillOval(x, y, 30, 30);
+		g.setColor(c);
+		
+		y += 5;
 	}
 
 	public static void main(String[] args) {
 		TankClient tc = new TankClient();
 		tc.launchFrame();
+	}
+	
+	private class PaintThread implements Runnable {
+
+		@Override
+		public void run() {
+			while(true){
+				repaint();
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			
+		}
+		
 	}
 
 }
